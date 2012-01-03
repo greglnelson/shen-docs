54c86c549d6dbe3f93093c295e9c8eab6758a82e
Bruno Deferrari <utizoc@gmail.com>
2012-01-03 17:13:24 -0200

    Switch to malcolm's logo.

--- learn_shen/conf.py
+++ learn_shen/conf.py
@@ -110,7 +110,7 @@ html_title = "Learn Shen"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/logo.png'
+html_logo = '_static/malcolm_logo_grey.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
